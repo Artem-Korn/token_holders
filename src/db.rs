e12db99a35dccae8ffb6fa1df48f0abdc6ae0f0c
Artem-Korn <Artem.Korniienko@cs.khpi.edu.ua>
2024-01-30 17:33:59 +0200

    Add GET request handlers

--- src/db.rs
+++ src/db.rs
@@ -1,44 +1,113 @@
 use anyhow::Result;
+use jsonapi::{api::*, jsonapi_model, model::*};
 use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, PgPool};
 use std::env;
 
 #[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
 pub struct Token {
-    pub token_id: i32,
+    pub id: i32,
     pub contract_addr: String,
     pub last_checked_block: i64,
     pub symbol: String,
     pub decimals: i16,
 }
 
-// #[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
-// pub struct Holder {
-//     pub holder_id: i32,
-//     pub holder_addr: String,
-// }
+#[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
+pub struct Holder {
+    pub id: i32,
+    pub holder_addr: String,
+}
 
-// #[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
-// pub struct Balance {
-//     pub holder_id: i32,
-//     pub token_id: i32,
-//     pub amount: String,
-// }
+#[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
+pub struct Balance {
+    pub id: String,
+    pub amount: String,
+}
 
 pub async fn init_db() -> Result<PgPool> {
+    jsonapi_model!(Token; "token");
+    jsonapi_model!(Holder; "holder");
+    jsonapi_model!(Balance; "balance");
+
     let database_url = env::var("DATABASE_URL")?;
     let connection_pool = PgPool::connect(&database_url).await?;
+
     // migrations
+
     Ok(connection_pool)
 }
 
-pub async fn token_by_id(connection_pool: &PgPool, token_id: i32) -> Result<Token> {
-    let sql = "SELECT token_id, 
+pub async fn all_token(connection_pool: &PgPool, offset: i64, limit: i64) -> Result<Vec<Token>> {
+    let sql = "SELECT token_id AS id, 
         encode(contract_addr, 'hex') AS contract_addr, 
-        last_checked_block, symbol, decimals FROM token WHERE token_id = $1";
+        last_checked_block, symbol, decimals FROM token ORDER BY symbol OFFSET $1 LIMIT $2";
 
     Ok(sqlx::query_as::<_, Token>(sql)
-        .bind(token_id)
+        .bind(offset)
+        .bind(limit)
+        .fetch_all(connection_pool)
+        .await?)
+}
+
+pub async fn holder_id_by_holder_addr(
+    connection_pool: &PgPool,
+    holder_addr: String,
+) -> Result<i32> {
+    let sql = "SELECT holder_id FROM holder WHERE holder_addr = decode($1, 'hex')";
+
+    Ok(sqlx::query_scalar::<_, i32>(sql)
+        .bind(holder_addr)
         .fetch_one(connection_pool)
+        .await?)
+}
+
+pub async fn token_id_by_contract_addr(
+    connection_pool: &PgPool,
+    contract_addr: String,
+) -> Result<i32> {
+    let sql = "SELECT token_id FROM token WHERE contract_addr = decode($1, 'hex')";
+
+    Ok(sqlx::query_scalar::<_, i32>(sql)
+        .bind(contract_addr)
+        .fetch_one(connection_pool)
+        .await?)
+}
+
+pub async fn all_balance_by_holder_id(
+    connection_pool: &PgPool,
+    holder_id: i32,
+    offset: i64,
+    limit: i64,
+    order: String,
+) -> Result<Vec<Balance>> {
+    let sql = format!("SELECT CONCAT(holder_id, '_', token_id) AS id, 
+        amount::text FROM balance WHERE holder_id = $1 ORDER BY balance.amount {} OFFSET $2 LIMIT $3",
+        order);
+
+    Ok(sqlx::query_as::<_, Balance>(sql.as_str())
+        .bind(holder_id)
+        .bind(offset)
+        .bind(limit)
+        .fetch_all(connection_pool)
+        .await?)
+}
+
+pub async fn all_balance_by_token(
+    connection_pool: &PgPool,
+    token_id: i32,
+    offset: i64,
+    limit: i64,
+    order: String,
+) -> Result<Vec<Balance>> {
+    let sql = format!("SELECT CONCAT(holder_id, '_', token_id) AS id, 
+        amount::text FROM balance WHERE token_id = $1 ORDER BY balance.amount {} OFFSET $2 LIMIT $3",
+        order);
+
+    Ok(sqlx::query_as::<_, Balance>(sql.as_str())
+        .bind(token_id)
+        .bind(offset)
+        .bind(limit)
+        .fetch_all(connection_pool)
         .await?)
 }
--- src/rest.rs
+++ src/rest.rs
@@ -1,21 +1,137 @@
-use axum::{extract::Path, http::StatusCode, routing::get, Extension, Json, Router};
+use std::collections::HashMap;
+
+use axum::extract::RawQuery;
+use axum::{http::StatusCode, routing::get, Extension, Json, Router};
+use jsonapi::model::{vec_to_jsonapi_document, JsonApiDocument};
+use jsonapi::query::{self, PageParams};
 use sqlx::PgPool;
 
-use crate::db::{token_by_id, Token};
+use crate::db::{
+    all_balance_by_holder_id, all_balance_by_token, all_token, holder_id_by_holder_addr,
+    token_id_by_contract_addr,
+};
+
+#[derive(Debug)]
+enum Filter {
+    Token(String),
+    Holder(String),
+}
+
+#[derive(Debug)]
+enum Include {
+    Token,
+    Holder,
+}
+
+fn pagination_parse(page_params: Option<PageParams>) -> Result<(i64, i64), StatusCode> {
+    match page_params {
+        Some(pagination) => {
+            if pagination.number >= 0 && pagination.size >= 0 {
+                Ok((pagination.number * pagination.size, pagination.size))
+            } else {
+                Err(StatusCode::BAD_REQUEST)
+            }
+        }
+        None => Ok((0, 0)),
+    }
+}
+
+fn filter_parse(filter_params: Option<HashMap<String, Vec<String>>>) -> Result<Filter, StatusCode> {
+    match filter_params {
+        Some(filter) => match (filter.get("token"), filter.get("holder")) {
+            (Some(token), None) => Ok(Filter::Token(token.first().unwrap().clone())),
+            (None, Some(holder)) => Ok(Filter::Holder(holder.first().unwrap().clone())),
+            (_, _) => Err(StatusCode::BAD_REQUEST),
+        },
+        None => Err(StatusCode::BAD_REQUEST),
+    }
+}
+
+fn include_parse(include_params: Option<Vec<String>>) -> Result<Include, StatusCode> {
+    match include_params {
+        Some(include) => match (
+            include.contains(&String::from("token")),
+            include.contains(&String::from("holder")),
+        ) {
+            (true, false) => Ok(Include::Token),
+            (false, true) => Ok(Include::Holder),
+            (_, _) => Err(StatusCode::BAD_REQUEST),
+        },
+        None => Err(StatusCode::BAD_REQUEST),
+    }
+}
+
+fn sort_parse(sort_params: Option<Vec<String>>) -> String {
+    match sort_params {
+        Some(sort) => match (
+            sort.contains(&String::from("-amount")),
+            sort.contains(&String::from("amount")),
+        ) {
+            (true, false) => String::from("DESC"),
+            (false, true) => String::from("ASC"),
+            (_, _) => String::from("ASC"),
+        },
+        None => String::from("ASC"),
+    }
+}
 
 pub fn create_router(connection_pool: PgPool) -> Router {
     Router::new()
-        .route("/:id", get(get_token))
+        .route("/tokens", get(tokens_handler))
+        .route("/balances", get(balances_handler))
         .layer(Extension(connection_pool))
 }
 
-async fn get_token(
+async fn tokens_handler(
     Extension(cp): Extension<PgPool>,
-    Path(token_id): Path<i32>,
-) -> Result<Json<Token>, StatusCode> {
-    if let Ok(token) = token_by_id(&cp, token_id).await {
-        Ok(Json(token))
+    RawQuery(query): RawQuery,
+) -> Result<Json<JsonApiDocument>, StatusCode> {
+    let query = query::Query::from_params(query.unwrap_or_default().as_str());
+    let (offset, limit) = pagination_parse(query.page)?;
+
+    if let Ok(tokens) = all_token(&cp, offset, limit).await {
+        Ok(Json(vec_to_jsonapi_document(tokens)))
     } else {
         Err(StatusCode::NOT_FOUND)
+    }
+}
+
+async fn balances_handler(
+    Extension(cp): Extension<PgPool>,
+    RawQuery(query): RawQuery,
+) -> Result<Json<JsonApiDocument>, StatusCode> {
+    let query = query::Query::from_params(query.unwrap_or_default().as_str());
+    let filter = filter_parse(query.filter)?;
+    let include = include_parse(query.include)?;
+    let (offset, limit) = pagination_parse(query.page)?;
+    let sort = sort_parse(query.sort);
+
+    match (filter, include) {
+        (Filter::Holder(holder_addr), Include::Token) => {
+            if let Ok(holder_id) = holder_id_by_holder_addr(&cp, holder_addr).await {
+                if let Ok(balances) =
+                    all_balance_by_holder_id(&cp, holder_id, offset, limit, sort).await
+                {
+                    Ok(Json(vec_to_jsonapi_document(balances)))
+                } else {
+                    Err(StatusCode::NOT_FOUND)
+                }
+            } else {
+                Err(StatusCode::NOT_FOUND)
+            }
+        }
+        (Filter::Token(contract_addr), Include::Holder) => {
+            if let Ok(token_id) = token_id_by_contract_addr(&cp, contract_addr).await {
+                if let Ok(balances) = all_balance_by_token(&cp, token_id, offset, limit, sort).await
+                {
+                    Ok(Json(vec_to_jsonapi_document(balances)))
+                } else {
+                    Err(StatusCode::NOT_FOUND)
+                }
+            } else {
+                Err(StatusCode::NOT_FOUND)
+            }
+        }
+        (_, _) => Err(StatusCode::BAD_REQUEST),
     }
 }
